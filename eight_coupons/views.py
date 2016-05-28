32d637d4a0f213daa9bc8d50cb5cb1f4bc900c65
pioneer <serge.tarkovski@gmail.com>
2016-05-29 01:21:35 +0300

    Added using actual search index in API

--- eight_coupons/views.py
+++ eight_coupons/views.py
@@ -1,5 +1,6 @@
 import json
 from aiohttp import web
+import snowballstemmer
 
 
 class SiteHandler:
@@ -11,7 +12,18 @@ class SiteHandler:
         search_params = {}
         search_str = request.GET.get('search')
         if search_str:
-            search_params["name"] = search_str
+            stemmer = snowballstemmer.stemmer("english")
+            # TODO: Add the same regex splitting and possibly HTML stripping
+            words = search_str.split()
+            game_ids = set()
+            for word in words:
+                word = word.lower()
+                stem = stemmer.stemWord(word)
+                index_item = await self.db.search_index.find_one({"stem": stem})
+                if index_item:
+                    game_ids = game_ids.union(index_item["game_ids"])
+            if game_ids:
+                search_params = {"id": {"$in": list(game_ids)}}
         games = []
         async for game in self.db.games.find(search_params):
             game.pop("_id")
